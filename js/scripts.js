bac2677f5df29d67685b00a672b948a590543e21
mfrantilla <mfrantilla@eagles.ewu.edu>
2016-06-08 07:33:01 -0700

    Github Pls
    Please upload.  Working on Windows so it's weird.

---
+++ js/scripts.js
@@ -0,0 +1,43 @@
+// First, load jQuery (required)
+// jQuery 2.2.2 loaded via `index.html`
+
+// Second, load Semantic UI JS (required)
+// @codekit-prepend "semantic.js";
+
+// Third, Add to Homescreen (optional)
+// @codekit-prepend "add-to-homescreen.js";
+
+// Third, load any plugins (optional)
+// @codekit-prepend "plugins.js";
+
+// My Scripts
+
+
+//Create Array of Quotes
+//This would be better as a database. But I'm not about to play around with PHP again.
+var quotes = [
+"Put on your coding hats!",
+"I found out that I spent all of last quarter using ‘Netflix and Chill’ wrong. I didn’t know what it meant",
+"What does Thirsty mean?",
+"Am I snarky?",
+"You guys don’t get into enough trouble as designers",
+"You guys gotta tell me what I can or can't use. I don't know any better.",
+"I’ve gotten into trouble using the eggplant emoji. Can’t use that.",
+"I promise this all comes together in the end",
+"I’m doing the whole 30, I can’t have rice. I’m Asian, do you know how hard it is for me to not have rice?",
+"Brent is my friend	",
+"I have a reminder to go pick up my daughter from school",
+"I don't handle any of my money, my wife keeps track of it all.",
+"Come to my office hours",
+"All of the good stuff goes in the curly braces",
+"Yes, I have my own ipsum.",
+"We're going to use Cornflowerblue",
+"You're used to Eaglenet not working, right?",
+"Someone spent a lot of money to put a stick on a building that shows you a color",
+"I spend a lot of time on dating sites",
+"Heads up, I didn't brush my teeth this morning",
+"I'm looking at a room full of people who have ideas"
+];
+
+
+console.log(quotes.size());
